28eeb0c96e7f078ad4d50f651afa64c8a641897b
Viacheslav <surzhenkoviacheslav@gmail.com>
2020-12-15 14:45:53 +0200

    RoomFixes

--- Assets/Scripts/DungeonGenerator/LevelGeneration.cs
+++ Assets/Scripts/DungeonGenerator/LevelGeneration.cs
@@ -8,234 +8,246 @@ namespace RPG.DungeonGenerator
 {
     public class LevelGeneration : MonoBehaviour
     {
-	    public GameObject spU, spD, spR, spL,
-		    spUD, spRL, spUR, spUL, spDR, spDL,
-		    spULD, spRUL, spDRU, spLDR, spUDRL;
+	    public GameObject[] T;
+	    public GameObject[] B;
+	    public GameObject[] R;
+	    public GameObject[] L;
+	    public GameObject[] TB;
+	    public GameObject[] RL;
+	    public GameObject[] TR;
+	    public GameObject[] TL;
+	    public GameObject[] BR;
+	    public GameObject[] BL;
+	    public GameObject[] TBL;
+	    public GameObject[] TRL;
+	    public GameObject[] TBR;
+	    public GameObject[] BRL;
+	    public GameObject[] TBRL;
 	    
 	    
         Vector2 worldSize = new Vector2(3,3);
-	Room[,] rooms;
-	List<Vector2> takenPositions = new List<Vector2>();
-	int gridSizeX, gridSizeY, numberOfRooms = 20;
-	public GameObject roomWhiteObj;
-	public Transform mapRoot;
-	void Start () {
-		if (numberOfRooms >= (worldSize.x * 2) * (worldSize.y * 2)){ // make sure we dont try to make more rooms than can fit in our grid
-			numberOfRooms = Mathf.RoundToInt((worldSize.x * 2) * (worldSize.y * 2));
+		Room[,] rooms;
+		List<Vector2> takenPositions = new List<Vector2>();
+		int gridSizeX, gridSizeY, numberOfRooms = 20;
+		public GameObject roomWhiteObj;
+		public Transform mapRoot;
+		void Start () {
+			if (numberOfRooms >= (worldSize.x * 2) * (worldSize.y * 2)){ // make sure we dont try to make more rooms than can fit in our grid
+				numberOfRooms = Mathf.RoundToInt((worldSize.x * 2) * (worldSize.y * 2));
+			}
+			gridSizeX = Mathf.RoundToInt(worldSize.x); //note: these are half-extents
+			gridSizeY = Mathf.RoundToInt(worldSize.y);
+			CreateRooms(); //lays out the actual map
+			SetRoomDoors(); //assigns the doors where rooms would connect
+			DrawMap(); //instantiates objects to make up a map
 		}
-		gridSizeX = Mathf.RoundToInt(worldSize.x); //note: these are half-extents
-		gridSizeY = Mathf.RoundToInt(worldSize.y);
-		CreateRooms(); //lays out the actual map
-		SetRoomDoors(); //assigns the doors where rooms would connect
-		DrawMap(); //instantiates objects to make up a map
-	}
-	void CreateRooms(){
+		void CreateRooms(){
 		//setup
-		rooms = new Room[gridSizeX * 2,gridSizeY * 2];
-		rooms[gridSizeX,gridSizeY] = new Room(Vector2.zero, 1);
-		takenPositions.Insert(0,Vector2.zero);
-		Vector2 checkPos = Vector2.zero;
-		//magic numbers
-		float randomCompare = 0.2f, randomCompareStart = 0.2f, randomCompareEnd = 0.01f;
-		//add rooms
-		for (int i =0; i < numberOfRooms -1; i++){
-			float randomPerc = ((float) i) / (((float)numberOfRooms - 1));
-			randomCompare = Mathf.Lerp(randomCompareStart, randomCompareEnd, randomPerc);
-			//grab new position
-			checkPos = NewPosition();
-			//test new position
-			if (NumberOfNeighbors(checkPos, takenPositions) > 1 && Random.value > randomCompare){
-				int iterations = 0;
-				do{
-					checkPos = SelectiveNewPosition();
-					iterations++;
-				}while(NumberOfNeighbors(checkPos, takenPositions) > 1 && iterations < 100);
-				if (iterations >= 50)
-					print("error: could not create with fewer neighbors than : " + NumberOfNeighbors(checkPos, takenPositions));
+			rooms = new Room[gridSizeX * 2,gridSizeY * 2];
+			rooms[gridSizeX,gridSizeY] = new Room(Vector2.zero, 1);
+			takenPositions.Insert(0,Vector2.zero);
+			Vector2 checkPos = Vector2.zero;
+			//magic numbers
+			float randomCompare = 0.2f, randomCompareStart = 0.2f, randomCompareEnd = 0.01f;
+			//add rooms
+			for (int i =0; i < numberOfRooms -1; i++){
+				float randomPerc = ((float) i) / (((float)numberOfRooms - 1));
+				randomCompare = Mathf.Lerp(randomCompareStart, randomCompareEnd, randomPerc);
+				//grab new position
+				checkPos = NewPosition();
+				//test new position
+				if (NumberOfNeighbors(checkPos, takenPositions) > 1 && Random.value > randomCompare){
+					int iterations = 0;
+					do{
+						checkPos = SelectiveNewPosition();
+						iterations++;
+					}while(NumberOfNeighbors(checkPos, takenPositions) > 1 && iterations < 100);
+					if (iterations >= 50)
+						print("error: could not create with fewer neighbors than : " + NumberOfNeighbors(checkPos, takenPositions));
+				}
+				//finalize position
+				rooms[(int) checkPos.x + gridSizeX, (int) checkPos.y + gridSizeY] = new Room(checkPos, 0);
+				takenPositions.Insert(0,checkPos);
+			}	
+		}
+		Vector2 NewPosition(){
+			int x = 0, y = 0;
+			Vector2 checkingPos = Vector2.zero;
+			do{
+				int index = Mathf.RoundToInt(Random.value * (takenPositions.Count - 1)); // pick a random room
+				x = (int) takenPositions[index].x;//capture its x, y position
+				y = (int) takenPositions[index].y;
+				bool UpDown = (Random.value < 0.5f);//randomly pick wether to look on hor or vert axis
+				bool positive = (Random.value < 0.5f);//pick whether to be positive or negative on that axis
+				if (UpDown){ //find the position bnased on the above bools
+					if (positive){
+						y += 1;
+					}else{
+						y -= 1;
+					}
+				}else{
+					if (positive){
+						x += 1;
+					}else{
+						x -= 1;
+					}
+				}
+				checkingPos = new Vector2(x,y);
+			}while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY); //make sure the position is valid
+			return checkingPos;
+		}
+		Vector2 SelectiveNewPosition(){ // method differs from the above in the two commented ways
+			int index = 0, inc = 0;
+			int x =0, y =0;
+			Vector2 checkingPos = Vector2.zero;
+			do{
+				inc = 0;
+				do{ 
+					//instead of getting a room to find an adject empty space, we start with one that only 
+					//as one neighbor. This will make it more likely that it returns a room that branches out
+					index = Mathf.RoundToInt(Random.value * (takenPositions.Count - 1));
+					inc++;
+				}while (NumberOfNeighbors(takenPositions[index], takenPositions) > 1 && inc < 100);
+				x = (int) takenPositions[index].x;
+				y = (int) takenPositions[index].y;
+				bool UpDown = (Random.value < 0.5f);
+				bool positive = (Random.value < 0.5f);
+				if (UpDown){
+					if (positive){
+						y += 1;
+					}else{
+						y -= 1;
+					}
+				}else{
+					if (positive){
+						x += 1;
+					}else{
+						x -= 1;
+					}
+				}
+				checkingPos = new Vector2(x,y);
+			}while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY);
+			if (inc >= 100){ // break loop if it takes too long: this loop isnt garuanteed to find solution, which is fine for this
+				print("Error: could not find position with only one neighbor");
 			}
-			//finalize position
-			rooms[(int) checkPos.x + gridSizeX, (int) checkPos.y + gridSizeY] = new Room(checkPos, 0);
-			takenPositions.Insert(0,checkPos);
-		}	
-	}
-	Vector2 NewPosition(){
-		int x = 0, y = 0;
-		Vector2 checkingPos = Vector2.zero;
-		do{
-			int index = Mathf.RoundToInt(Random.value * (takenPositions.Count - 1)); // pick a random room
-			x = (int) takenPositions[index].x;//capture its x, y position
-			y = (int) takenPositions[index].y;
-			bool UpDown = (Random.value < 0.5f);//randomly pick wether to look on hor or vert axis
-			bool positive = (Random.value < 0.5f);//pick whether to be positive or negative on that axis
-			if (UpDown){ //find the position bnased on the above bools
-				if (positive){
-					y += 1;
-				}else{
-					y -= 1;
-				}
-			}else{
-				if (positive){
-					x += 1;
-				}else{
-					x -= 1;
-				}
+			return checkingPos;
+		}
+		int NumberOfNeighbors(Vector2 checkingPos, List<Vector2> usedPositions){
+			int ret = 0; // start at zero, add 1 for each side there is already a room
+			if (usedPositions.Contains(checkingPos + Vector2.right)){ //using Vector.[direction] as short hands, for simplicity
+				ret++;
 			}
-			checkingPos = new Vector2(x,y);
-		}while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY); //make sure the position is valid
-		return checkingPos;
-	}
-	Vector2 SelectiveNewPosition(){ // method differs from the above in the two commented ways
-		int index = 0, inc = 0;
-		int x =0, y =0;
-		Vector2 checkingPos = Vector2.zero;
-		do{
-			inc = 0;
-			do{ 
-				//instead of getting a room to find an adject empty space, we start with one that only 
-				//as one neighbor. This will make it more likely that it returns a room that branches out
-				index = Mathf.RoundToInt(Random.value * (takenPositions.Count - 1));
-				inc ++;
-			}while (NumberOfNeighbors(takenPositions[index], takenPositions) > 1 && inc < 100);
-			x = (int) takenPositions[index].x;
-			y = (int) takenPositions[index].y;
-			bool UpDown = (Random.value < 0.5f);
-			bool positive = (Random.value < 0.5f);
-			if (UpDown){
-				if (positive){
-					y += 1;
-				}else{
-					y -= 1;
-				}
-			}else{
-				if (positive){
-					x += 1;
-				}else{
-					x -= 1;
-				}
+			if (usedPositions.Contains(checkingPos + Vector2.left)){
+				ret++;
 			}
-			checkingPos = new Vector2(x,y);
-		}while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY);
-		if (inc >= 100){ // break loop if it takes too long: this loop isnt garuanteed to find solution, which is fine for this
-			print("Error: could not find position with only one neighbor");
+			if (usedPositions.Contains(checkingPos + Vector2.up)){
+				ret++;
+			}
+			if (usedPositions.Contains(checkingPos + Vector2.down)){
+				ret++;
+			}
+			return ret;
 		}
-		return checkingPos;
-	}
-	int NumberOfNeighbors(Vector2 checkingPos, List<Vector2> usedPositions){
-		int ret = 0; // start at zero, add 1 for each side there is already a room
-		if (usedPositions.Contains(checkingPos + Vector2.right)){ //using Vector.[direction] as short hands, for simplicity
-			ret++;
-		}
-		if (usedPositions.Contains(checkingPos + Vector2.left)){
-			ret++;
-		}
-		if (usedPositions.Contains(checkingPos + Vector2.up)){
-			ret++;
-		}
-		if (usedPositions.Contains(checkingPos + Vector2.down)){
-			ret++;
-		}
-		return ret;
-	}
-	void DrawMap(){
-		foreach (Room room in rooms){
+		void DrawMap(){
+			foreach (Room room in rooms){
 			
-			Debug.Log("Creating");
-			if (room == null){
-				continue; //skip where there is no room
-			}
+				Debug.Log("Creating");
+				if (room == null){
+					continue; //skip where there is no room
+				}
 			
-			Debug.Log("Rooms");
-			Vector2 drawPos = room.gridPos;
-			drawPos.x *= 16;//aspect ratio of map sprite
-			drawPos.y *= 15;
-			//create map obj and assign its variables
-			var r = Instantiate(PickObject(room), drawPos, Quaternion.identity);
-			r.gameObject.transform.parent = mapRoot;
+				Debug.Log("Rooms");
+				Vector2 drawPos = room.gridPos;
+				drawPos.x *= 16;//aspect ratio of map sprite
+				drawPos.y *= 15;
+				//create map obj and assign its variables
+				var r = Instantiate(PickObject(room), drawPos, Quaternion.identity);
+				r.gameObject.transform.parent = mapRoot;
+			}
 		}
-	}
-	void SetRoomDoors(){
-		for (int x = 0; x < ((gridSizeX * 2)); x++){
-			for (int y = 0; y < ((gridSizeY * 2)); y++){
-				if (rooms[x,y] == null){
-					continue;
-				}
-				Vector2 gridPosition = new Vector2(x,y);
-				if (y - 1 < 0){ //check above
-					rooms[x,y].doorBot = false;
-				}else{
-					rooms[x,y].doorBot = (rooms[x,y-1] != null);
-				}
-				if (y + 1 >= gridSizeY * 2){ //check bellow
-					rooms[x,y].doorTop = false;
-				}else{
-					rooms[x,y].doorTop = (rooms[x,y+1] != null);
-				}
-				if (x - 1 < 0){ //check left
-					rooms[x,y].doorLeft = false;
-				}else{
-					rooms[x,y].doorLeft = (rooms[x - 1,y] != null);
-				}
-				if (x + 1 >= gridSizeX * 2){ //check right
-					rooms[x,y].doorRight = false;
-				}else{
-					rooms[x,y].doorRight = (rooms[x+1,y] != null);
+		void SetRoomDoors(){
+			for (int x = 0; x < ((gridSizeX * 2)); x++){
+				for (int y = 0; y < ((gridSizeY * 2)); y++){
+					if (rooms[x,y] == null){
+						continue;
+					}
+					Vector2 gridPosition = new Vector2(x,y);
+					if (y - 1 < 0){ //check above
+						rooms[x,y].doorBot = false;
+					}else{
+						rooms[x,y].doorBot = (rooms[x,y-1] != null);
+					}
+					if (y + 1 >= gridSizeY * 2){ //check bellow
+						rooms[x,y].doorTop = false;
+					}else{
+						rooms[x,y].doorTop = (rooms[x,y+1] != null);
+					}
+					if (x - 1 < 0){ //check left
+						rooms[x,y].doorLeft = false;
+					}else{
+						rooms[x,y].doorLeft = (rooms[x - 1,y] != null);
+					}
+					if (x + 1 >= gridSizeX * 2){ //check right
+						rooms[x,y].doorRight = false;
+					}else{
+						rooms[x,y].doorRight = (rooms[x+1,y] != null);
+					}
 				}
 			}
 		}
-	}
 	
-	GameObject PickObject(Room room){ //picks correct sprite based on the four door bools
-		if (room.doorTop){
+		GameObject PickObject(Room room){ //picks correct sprite based on the four door bools
+			if (room.doorTop){
+				if (room.doorBot){
+					if (room.doorRight){
+						if (room.doorLeft){
+							return TBRL[Random.Range(0,TBRL.Length)];
+						}else{
+							return TBR[Random.Range(0,TBR.Length)];
+						}
+					}else if (room.doorLeft){
+						return TBL[Random.Range(0,TBL.Length)];
+					}else{
+						return TB[Random.Range(0,TB.Length)];
+					}
+				}else{
+					if (room.doorRight){
+						if (room.doorLeft){
+							return TRL[Random.Range(0,TRL.Length)];
+						}else{
+							return TR[Random.Range(0,TR.Length)];
+						}
+					}else if (room.doorLeft){
+						return TL[Random.Range(0,TL.Length)];
+					}else{
+						return T[Random.Range(0,T.Length)];
+					}
+				}
+				return null;
+			}
 			if (room.doorBot){
 				if (room.doorRight){
-					if (room.doorLeft){
-						return spUDRL;
+					if(room.doorLeft){
+						return BRL[Random.Range(0,BRL.Length)];
 					}else{
-						return spDRU;
+						return BR[Random.Range(0,BR.Length)];
 					}
 				}else if (room.doorLeft){
-					return spULD;
+					return BL[Random.Range(0,BL.Length)];
 				}else{
-					return spUD;
+					return B[Random.Range(0,B.Length)];
 				}
-			}else{
-				if (room.doorRight){
-					if (room.doorLeft){
-						return spRUL;
-					}else{
-						return spUR;
-					}
-				}else if (room.doorLeft){
-					return spUL;
-				}else{
-					return spU;
-				}
+				return null;
 			}
-			return null;
-		}
-		if (room.doorBot){
 			if (room.doorRight){
-				if(room.doorLeft){
-					return spLDR;
+				if (room.doorLeft){
+					return RL[Random.Range(0,RL.Length)];
 				}else{
-					return spDR;
+					return R[Random.Range(0,R.Length)];
 				}
-			}else if (room.doorLeft){
-				return spDL;
 			}else{
-				return spD;
+				return L[Random.Range(0,L.Length)];
 			}
-			return null;
 		}
-		if (room.doorRight){
-			if (room.doorLeft){
-				return spRL;
-			}else{
-				return spR;
-			}
-		}else{
-			return spL;
-		}
-	}
     }
 }
---
+++ Assets/Scripts/DungeonGenerator/RoomManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace RPG.DungeonGenerator
+{
+    public class RoomManager : MonoBehaviour
+    {
+        [SerializeField] private Tilemap obstacleMap;
+        [SerializeField] private Tilemap groundMap;
+
+        public bool[,] _tiles = new bool[16,15];
+
+        private void Start()
+        {
+            if (obstacleMap == null || groundMap == null)
+            {
+                Debug.LogError("Maps == null" + gameObject.name);
+                return;
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    if (obstacleMap.GetTile(new Vector3Int(i, j, 0)) == null &&
+                        groundMap.GetTile(new Vector3Int(i, j, 0)) != null)
+                    {
+                        _tiles[i,j] = true;
+                    }
+                    else
+                    {
+                        _tiles[i,j] = false;
+                    }
+                }
+            }
+        }
+    }
+}
